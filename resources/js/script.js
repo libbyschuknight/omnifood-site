a1c2f93757e55329ada93e564a267324c871423d
Libby Schumacher-Knight <libby@fluxfederation.com>
2018-02-15 14:33:19 +1300

    Add prefixers for css and get jquery working

---
+++ resources/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+
+
+});
+
+
+
+
+
+
+// Simple jquery and how to do in vanilla js
+
+// jQuery
+// $(document).ready(function () {
+//
+//   $("h2").click(function () {
+//     $(this).css("background-color", "#ff0000");
+//   });
+//
+//
+// });
+
+// Vanilla
+// document.addEventListener('DOMContentLoaded', function() {
+//   [].forEach.call(document.querySelectorAll('h2'), function(el) {
+//     el.addEventListener('click', function() {
+//       el.style.backgroundColor = '#ff0000';
+//     })
+//   })
+// })
