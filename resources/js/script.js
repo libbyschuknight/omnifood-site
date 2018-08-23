7eb51fb4af8c86ccea2ea3116c8f0c2210e91656
Libby Schumacher-Knight <libby@fluxfederation.com>
2018-08-23 15:51:41 +1200

    Fix nav, add favicon
    @ lecture 69

--- resources/js/script.js
+++ resources/js/script.js
@@ -94,18 +94,62 @@ $('a[href*="#"]')
 
 
   // Mobile navigation
-  $('.js--nav-icon').click(function() {
+  // New code from https://jsfiddle.net/d83ykzku/1/
+  $('.js--nav-icon, .js--main-nav a, .logo-black').click(function (element) {
     var nav = $('.js--main-nav');
     var icon = $('.js--nav-icon i');
-    nav.slideToggle(200);
+
+    //Gets the class name of the element that triggered the event
+    var clicked = element.target.className;
+
+    //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+    if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black')
+      return;
+
+    //Opens and closes the menu
+    if ($(window).width() < 768) {
+      nav.slideToggle(200);
+    }
+
+    //Changes icon states of the menu button
     if (icon.hasClass('ion-navicon-round')) {
       icon.addClass('ion-close-round');
       icon.removeClass('ion-navicon-round');
     } else {
       icon.addClass('ion-navicon-round');
       icon.removeClass('ion-close-round');
-    };
+    }
   });
+
+
+  $(window).resize(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    if ($(window).width() > 767) {
+      nav.css("display", "block");
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      nav.css("display", "none");
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+
+  });
+  // OLD CODE from tutorial
+  // $('.js--nav-icon').click(function() {
+  //   var nav = $('.js--main-nav');
+  //   var icon = $('.js--nav-icon i');
+  //   nav.slideToggle(200);
+  //   if (icon.hasClass('ion-navicon-round')) {
+  //     icon.addClass('ion-close-round');
+  //     icon.removeClass('ion-navicon-round');
+  //   } else {
+  //     icon.addClass('ion-navicon-round');
+  //     icon.removeClass('ion-close-round');
+  //   };
+  // });
 
 });
 
